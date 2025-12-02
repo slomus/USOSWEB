567dc7d1ad40e4609609cf0d03099cdf3feaafd8
Karol <karol@uniqsoft.pl>
2025-12-02 15:18:43 +0100

    fix: correct build tag syntax in tools.go and update golangci-lint config

--- src/backend/tools/tools.go
+++ src/backend/tools/tools.go
@@ -1,10 +1,10 @@
-go:build tools
+//go:build tools
 // +build tools
 
 package tools
 
 import (
-    _ "google.golang.org/protobuf/cmd/protoc-gen-go"
-    _ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
-    _ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
+	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
